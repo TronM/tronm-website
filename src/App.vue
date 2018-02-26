a69d079b2b298ae96ea12285f0e1db10aa70a7bf
wellfrog <wellfrog16@gmail.com>
2018-02-26 18:07:29 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <!-- <img src="./assets/logo.png"> -->
+        <router-view/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app'
+};
+</script>
+
+<style lang="less">
+html, body {
+    padding: 0;
+    margin: 0;
+}
+
+body {
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+    font-size: 14px;
+}
+</style>
